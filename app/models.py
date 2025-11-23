1809afefaec6c6cf9ef6164cf16a2c8bc5d1791d
Kriti Khemani <kritikhemani04@gmail.com>
2025-11-23 13:00:27 +0530

    feat: Adding User class to models.py

---
+++ app/models.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class User(Base):
+    _tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
