3f9f95edac80e10041dc7d9f925329eaf1b5554f
Kriti Khemani <kritikhemani04@gmail.com>
2025-11-24 14:02:55 +0530

    feat: Update class Note in database.py

--- app/models.py
+++ app/models.py
@@ -10,3 +10,8 @@ class User(Base):
     
 class Note(Base):
     __tablename__ = "notes"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    
