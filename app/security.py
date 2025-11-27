4f1296b2fb178693119a7c3e995efa580f3a2fce
Kriti Khemani <kritikhemani04@gmail.com>
2025-11-27 17:18:14 +0530

    Adding verified_password function to security.py

--- app/security.py
+++ app/security.py
@@ -11,3 +11,6 @@ REFRESH_EXPIRE_DAYS = 7
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
+def verify_password(password: str, hashed: str) -> bool:
+    return pwd_context.verify(password, hashed)
