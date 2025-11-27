0e71c16ea95ca32be1c9c34ae806cbe18dc4e0aa
Kriti Khemani <kritikhemani04@gmail.com>
2025-11-27 16:19:01 +0530

    feat: Update class User in models.py

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(String, default="user")
     is_active = Column(Boolean, default=True)
     
     
